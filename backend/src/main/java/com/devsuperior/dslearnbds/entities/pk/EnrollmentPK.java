80602f74e0a65d7c10b5fe1c754fa478b2665d65
lhp97 <lhdprado@gmail.com>
2022-12-23 13:12:09 -0300

    Enrollment

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/pk/EnrollmentPK.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dslearnbds.entities.pk;
+
+import com.devsuperior.dslearnbds.entities.Offer;
+import com.devsuperior.dslearnbds.entities.User;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+public class EnrollmentPK implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public EnrollmentPK() {}
+
+    public EnrollmentPK(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 1', 'O inicio', 1, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2', 'Parte 2', 2, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg',1,1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 3', 'Parte 3', 3, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg',1,2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2022-12-23T13:32:00', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2022-12-23T13:32:00', null, true, false);
